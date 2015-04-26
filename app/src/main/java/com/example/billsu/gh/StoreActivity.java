ec7dfbb1e215ea2646703bba08ca96f8ae3c4e05
Montie319 <lmb2ma@virginia.edu>
2015-04-26 19:01:34 -0400

    Shitload

--- app/src/main/java/com/example/billsu/gh/StoreActivity.java
+++ app/src/main/java/com/example/billsu/gh/StoreActivity.java
@@ -17,11 +17,19 @@ import android.view.View;
 public class StoreActivity extends Activity {
 
     MediaPlayer storeSong;
+    double money;
+    double battery;
+    double fear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.store_activity);
+        Bundle bundle = getIntent().getExtras();
+        money = bundle.getDouble("money");
+        fear = bundle.getDouble("fear");
+        battery = bundle.getDouble("battery");
+
 
         storeSong = MediaPlayer.create(StoreActivity.this, R.raw.store_sound);
         storeSong.start();
@@ -30,15 +38,27 @@ public class StoreActivity extends Activity {
 
 
     public void fearUpgrade(View v) {
+        if (money>= 10){
+            fear=-300;
+            money=-10;
+        }
+
+
 
     }
 
     public void batteryUpgrade(View v) {
+        if (money>= 2){
+            battery+=600;
+            money=-2;
+        }
 
     }
 
     public void startGame(View V) {
         storeSong.stop();
+
+
         this.finish(); //closes store, starts up game again
     }
 
